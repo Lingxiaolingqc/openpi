ce26bc2e520e7900596063b067fd055d63297e7a
Lingxiaolingqc <Chuan.lingyun@outlook.com>
2026-08-12 15:58:35 +0800

    adjust autogen reference ray hit to 0.044

--- examples/so101/red_cube_to_box_task/autogen_reference_state_machine.py
+++ examples/so101/red_cube_to_box_task/autogen_reference_state_machine.py
@@ -62,7 +62,7 @@ class RedCubeToBoxAutogenReferenceStateMachine(StateMachineBase):
 
     GREEN_RAY_ORIGIN_OFFSET = (CUBE_HALF_HEIGHT, 0.0, -0.04)
     GREEN_RAY_DIRECTION = (0.0, 0.0, -1.0)
-    GREEN_RAY_MAX_HIT_DISTANCE = CUBE_HALF_HEIGHT + 0.048
+    GREEN_RAY_MAX_HIT_DISTANCE = CUBE_HALF_HEIGHT + 0.044
     LOCAL_RAY_AXES: ClassVar[dict[str, tuple[float, float, float]]] = {
         "+x": (1.0, 0.0, 0.0),
         "-x": (-1.0, 0.0, 0.0),
